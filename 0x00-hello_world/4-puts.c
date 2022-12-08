1ce6ee0c1e1622718f366722c070a9ad58f516eb
Fablaurenz <lawrenceejim142@gmail.com>
2022-12-08 05:05:08 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-gcc $CFILE -o cisfun
+#include <stdio.h>
+/**
+ * main -Print a line with puts
+ *
+ * Return 0
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	Return(0);
+}
--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
- * main - write a line of code using puts
+ * main -write a line of code using puts
  *
  * Return 0
  * /
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	Return (0)
+	Return (0);
 }
