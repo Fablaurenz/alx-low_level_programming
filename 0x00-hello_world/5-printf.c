f825f9730bc2ef5fc0b8f197426ca47ce71634d2
Fablaurenz <lawrenceejim142@gmail.com>
2022-12-08 07:23:19 -0800

    Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,10 @@
 /**
  * main -Entry point
  * Description: prints "with proper grammar, but the outcome is a piece of art"
- * return: Always 0 (Success)
+ * return: 0
 */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
