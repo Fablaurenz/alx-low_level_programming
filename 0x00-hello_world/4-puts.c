b1e1f538bd83bfa961be0acf72fda3f6b7ef12ea
Fablaurenz <lawrenceejim142@gmail.com>
2022-12-08 05:18:01 -0800

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main -Print a line with puts
- *
- * Return 0
+ * main -Entry Point
+ * Description: prints "\"Programming is like building a multilingual puzzle"
+ * Return: Always 0 (Success)
 */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	Return(0);
+	Return (0);
 }
