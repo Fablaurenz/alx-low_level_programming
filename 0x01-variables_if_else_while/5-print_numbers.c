71b39b429e0a7b1b4f496402eb777690d481fbf5
Fablaurenz <lawrenceejim142@gmail.com>
2022-12-11 06:54:25 -0800

    Print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Print numbers fom 0 to 9
+ *
+ * Return: A;ways 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		printf("%d\n", i);
+	}
+
+	return (0);
+}
