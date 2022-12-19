a49df7111a6f8422cba90c9758339420c43e0e3e
Fablaurenz <lawrenceejim142@gmail.com>
2022-12-19 04:39:09 -0800

    Strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int kongi = o;
+
+	while (*s != '\0')
+	{
+		kongi++;
+		s++;
+	}
+	return (kongi);
+}
